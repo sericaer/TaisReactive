f3d3388e348fffc6e91cdeb52c140f5f464d0590
sericaer <sericaer@outlook.com>
2022-03-04 18:25:41 +0800

    show tax tooltip

---
+++ Assets/Scripts/LazyUpdateTooltipTrigger.cs
@@ -0,0 +1,30 @@
+﻿using ModelShark;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TooltipTrigger))]
+public class LazyUpdateTooltipTrigger : MonoBehaviour
+{
+    public Func<IEnumerable<(string paramName, string text)>> funcGenerateTextInfo;
+
+    private TooltipTrigger tooltipTrigger;
+    void Start()
+    {
+        tooltipTrigger = GetComponent<TooltipTrigger>();
+        tooltipTrigger.actionBeforPopup = BeforPopup;
+    }
+
+    void BeforPopup()
+    {
+        if (funcGenerateTextInfo != null)
+        {
+            var textInfo = funcGenerateTextInfo();
+            foreach (var info in textInfo)
+            {
+                tooltipTrigger.SetText(info.paramName, info.text);
+            }
+        }
+    }
+}
--- Assets/Scripts/MainScene/PlayerTax.cs
+++ Assets/Scripts/MainScene/PlayerTax.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Tais.GSessions;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,12 +10,18 @@ class PlayerTax : RxMonoBehaviour
 {
     public Text stock;
     public Text perMonth;
+    public LazyUpdateTooltipTrigger tooltipTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
         dataBind.BindText(GSession.inst.taxMgr, x => x.stock, stock);
-        dataBind.BindText(GSession.inst.taxMgr, x => x.taxPerMonth, perMonth, (v)=> $"(" + v.ToString("+0;-#") +")");
+        dataBind.BindText(GSession.inst.taxMgr, x => x.taxPerMonth, perMonth, (v)=> "(" + v.ToString("+0;-#") +")");
+        tooltipTrigger.funcGenerateTextInfo = () =>
+        {
+            var info = string.Join("\n", GSession.inst.taxMgr.taxSourcesPerMonth.Items.Select(x => x.label + " " + x.value.ToString("+0;-#")));
+            return new (string, string)[] { ("BodyText", info) };
+        };
     }
 
     // Update is called once per frame
